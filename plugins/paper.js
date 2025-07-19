711ccf92b2ba2660d82a5ed84be9917aebd2f0ac
danuwac <harshawhatsapp21@gmail.com>
2025-07-20 02:48:22 +0530

    Update paper.js

--- plugins/paper.js
+++ plugins/paper.js
@@ -12,7 +12,7 @@ async (conn, mek, m, {
     from, reply
 }) => {
     try {
-        const bannerImg = 'https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/Alive.png?raw=true';
+        const bannerImg = 'https://github.com/DANUWA-MD/DANUWA-BOT/blob/main/images/educational__zone.png?raw=true';
 
         const channelJid = '120363418166326365@newsletter'; 
         const channelName = '🍁 ＤＡＮＵＷＡ－ 〽️Ｄ 🍁';
@@ -20,40 +20,44 @@ async (conn, mek, m, {
 
         const caption = `
 ╭─────── ⭓ ⭓ ⭓  ─────────╮
-│   🎓 𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍𝐀𝐋 𝐙𝐎𝐍𝐄 🎓   │
+│   🎓 𝗘𝗗𝗨𝗖𝗔𝗧𝗜𝗢𝗡𝗔𝗟 𝗭𝗢𝗡𝗘 🎓   │
 ╰──────────────⟡───────╯
 │ 📘 *Term Test Papers (Grades 6–11)*
-│    └ Use: *.govdoc grade subject year*
-│    └ Ex: *.govdoc 10 history 2024*
+│    └ Use: *.termtest grade subject*
+│    └ Ex: *.termtest grade 10 history*
 │──────────────────────⬣
 │ 📕 *O/L Past Papers*
-│    └ Use: *.pastpapers year subject*
-│    └ Ex: *.pastpapers 2023 maths*
+│    └ Use: *.ol subject*
+│    └ Ex: *.ol maths*
 │──────────────────────⬣
 │ 📗 *A/L Past Papers*
-│    └ Use: *.pastpapers physics*
+│    └ Use: *.al physics*
 │    └ Short Forms: sft, et, bst
-│    └ Ex: *.pastpapers bst*
+│    └ Ex: *.al bst*
 │──────────────────────⬣
 │ 📙 *O/L & A/L Model Papers*
-│    └ Use: *.modelpapers ol/al subject*
-│    └ Ex: *.modelpapers ol science*
+│    └ Use: *.model ol/al subject*
+│    └ Ex: *.model o/l science*
 │──────────────────────⬣
 │ 📚 *School Textbooks (Grade 1–13)*
-│    └ Use: *.textbook grade subject*
-│    └ Ex: *.textbook 6 tamil*
+│    └ Use: *.textbook grade*
+│    └ Ex: *.textbook 6*
+│──────────────────────⬣
+│ 🎥 *Subject Video Playlists*
+│    └ Use: *.subjectvideos al/ol subject*
+│    └ Ex: *.subjectvideos a/l biology*
 │──────────────────────⬣
 │ 📑 *Syllabus*
-│    └ Use: *.syllabus grade subject*
-│    └ Ex: *.syllabus 11 maths*
+│    └ Use: *.syllabus grade*
+│    └ Ex: *.syllabus 11*
 │──────────────────────⬣
 │ 📑 *Teachers' Guides*
-│    └ Use: *.tguide grade subject*
-│    └ Ex: *.tguide 11 maths*
+│    └ Use: *.tguide grade*
+│    └ Ex: *.tguide 11*
 │──────────────────────⬣
 │ 📰 *Government Gazette Downloads*
-│    └ Use: *.gazette keyword*
-│    └ Ex: *.gazette teaching*
+│    └ Use: *.gazette*
+│    └ Ex: *.gazette*
 ╰──────────────────────╯
 ⚙️ Made with ❤️ by
 ╰🔥 𝘿𝘼𝙉𝙐𝙆𝘼 𝘿𝙄𝙎𝘼𝙉𝘼𝙔𝘼𝙆𝘼 🔥`;
